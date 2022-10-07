0c326f55fff04a680037a91e65f4f21defda6619
yass <yassine.lakhdari2@edu.esiee.fr>
2022-10-07 13:34:16 +0200

    ajout fin mot bug

--- find_motive/find_motive.cpp
+++ find_motive/find_motive.cpp
@@ -10,25 +10,28 @@ int find_motive(string filename, string motive){
     if(!file.is_open()){
         cout << "The file " << filename << " could not be opened." << endl;
         return 1;
-    }   
-    int pos(0);
+    }
     int nb_it(0);
-    while(getline(file, line)){
-        for(int i = 0; i<line.length(); i++){
-            if(pos == motive.length()){
-                nb_it++;
-                pos = -1;
-            }
-            else if(line[i] == ' '){
-                pos = 0;
-            }
-            else if(line[i] == motive[pos] && pos != -1){
+    int nb(0);
+    int pos(0);
+    string mot;
+    file >> mot;
+    while(!file.eof()){
+        for(int i = 0; i<mot.length(); i++){
+            if(mot[i] == motive[pos]){
                 pos++;
             }
             else{
                 pos = 0;
             }
+            if(pos == motive.length()){
+                nb_it++;
+                pos = 0;
+                break;
+            }
         }
+        nb++;
+        file >> mot;
     }
     cout << "The file " << filename << " contains " << nb_it << " words containing the motive " << motive << endl;
     return 0;
