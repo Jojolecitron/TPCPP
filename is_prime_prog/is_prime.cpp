0a7ff40e53992e030b578ae40d0f2b74ffa51a46
yass <yassine.lakhdari2@edu.esiee.fr>
2022-10-10 09:33:24 +0200

    tt

--- is_prime_prog/is_prime.cpp
+++ is_prime_prog/is_prime.cpp
@@ -3,8 +3,8 @@
 #include <ctime>
 using namespace std;
 
-int is_prime(unsigned long long int value){
-    int i(0);
+int is_prime(long long int value){
+    unsigned long long int i(0);
     if (value == 1){
         return 0;
     }
@@ -14,22 +14,13 @@ int is_prime(unsigned long long int value){
     else if (value%2 == 0){
         return 0;
     }
-    for(i = 3; i<sqrt(value); i+=2){
+    for(i = 3; i<=sqrt(value); i+=2){
         if(!(value%i)){
             return 0;
         }
     }
     return 1;
 }
-
-int time_is_prime(void){
-    int start = time(NULL);
-    for(int i=0; i<1000000; i++){
-        cout << i << " is prime; " << (is_prime(i) ? "True": "False") << endl;
-    }
-    return time(NULL) - start;
-}
-
 
 int main(){
     unsigned long long int n;
@@ -38,7 +29,5 @@ int main(){
         cout << n << " is a prime: " << (is_prime(n) ? "True": "False") << '\n';
         cin >> n;
     }while(cin);
-    // int t = time_is_prime();
-    // cout << "finished in : " << t << " seconds" << endl;
     return 0;
 }
